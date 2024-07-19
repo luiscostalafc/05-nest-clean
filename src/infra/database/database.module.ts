55c3cfcf283a55b081e09b523692ee6940dc6186
Luis Costa <luiscostalafc@gmail.com>
2024-07-19 06:17:19 -0300

    feat: create database module

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository';
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository';
+import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository';
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository';
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository';
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswersRepository,
+  ],
+  exports: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswersRepository,
+  ],
 })
 export class DatabaseModule {}
